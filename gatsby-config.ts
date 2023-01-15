2a15fa34bdee6e83d36ec9c6a89e8fb3e51038bf
Andrew McCallum <andrew.m@affinity.ad>
2023-01-16 10:20:13 +1100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,25 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Playground`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "http://wordpressplayground.local/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
+
+export default config;
